793fe4c908090599886a8e2ad9765a72ba3dad69
Joel_Wilhelm <drakonnaskaiba@gmail.com>
2017-04-12 16:48:56 -0400

    Add files via upload

--- SRC/JSON/Tester.java
+++ SRC/JSON/Tester.java
@@ -4,7 +4,7 @@ public class Tester{
    
         JSON myJ = new JSON("0123456789", "Testing", "Monday", "1","2", "3","4", 
                 "5","6","77","888", "9999","1", "2","3", "800", "Friday", "Noon",
-                "5", 1, 1, 1, 5);
+                "5", 1, 1, 1, 5, 1);
         
         System.out.println(myJ.get_JSONSTRING());
         
